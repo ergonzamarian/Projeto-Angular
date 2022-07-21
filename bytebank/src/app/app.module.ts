879978b4d5c5b63e43213cdca22ea1d8e60437bf
Ergon Zamarian Lima <ergonzamarian@gmail.com>
2022-07-20 20:57:17 -0400

    Criando novo component através no ng generate <nome do component>

--- bytebank/src/app/app.module.ts
+++ bytebank/src/app/app.module.ts
@@ -1,14 +1,20 @@
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferenca.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ExtratoComponent } from './extrato/extrato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovaTransferenciaComponent,
+    ExtratoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ bytebank/src/app/nova-transferencia/nova-transferenca.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Output } from "@angular/core";
+
+@Component({
+    selector: 'app-nova-transferencia',
+    templateUrl: './nova-transferencia.component.html',
+    styleUrls: ['./nova-transferencia.component.scss']
+})
+
+export class NovaTransferenciaComponent {
+
+    @Output() aoTransferir = new EventEmitter<any>();
+
+    valor: number;
+    destino: number;
+
+    transferir(){
+      console.log('Solicitada nova transferência');
+      const valorEmitir = {valor: this.valor, destino: this.destino}
+      this.aoTransferir.emit(valorEmitir);
+      this.limparCampos();
+    }
+
+    limparCampos(){
+      this.valor=0;
+      this.destino=0;
+    }
+}
